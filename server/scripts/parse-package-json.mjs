a839e97a5da01d010339c6873900ee4c494afd32
chaikangduo <gunhongchen@163.com>
2025-08-09 15:50:33 +0800

    feat:server服务器pm2启动es模式问题

--- server/scripts/parse-package-json.mjs
+++ server/scripts/parse-package-json.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const packagePath = resolve(__dirname, '../package.json')
+const packagePath = resolve(__dirname, '../../package.json')
 readFile(packagePath, {encoding: 'utf-8'}, (err, data) => {
   if(err) {
     throw Error('文件读取失败', err)
--- server/scripts/server.js
+++
@@ -1,8 +0,0 @@
-// start.cjs
-const path = require('path')
-import(path.resolve(__dirname, '../server.mjs'))
-  .then(mod => mod.default?.())
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
