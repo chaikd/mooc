9c9a561e3fa93e3b64ae1b896599fd2e89cfc1f6
chaikangduo <gunhongchen@163.com>
2025-08-09 14:59:11 +0800

    feat:server服务器pm2启动es模式问题

--- server/scripts/parse-package-json.cjs
+++ server/scripts/parse-package-json.cjs
@@ -1,9 +1,10 @@
 import { readFile, writeFileSync } from "fs";
 
-readFile('./package.json', {encoding: 'utf-8'}, (err, data) => {
+const packagePath = resolve(__dirname, '../dist/package.json')
+readFile(packagePath, {encoding: 'utf-8'}, (err, data) => {
   if(err) {
     throw Error('文件读取失败', err)
   }
   const str = data.replace('"@mooc/db-shared": "workspace:^",', '')
-  writeFileSync('./package.json', str)
+  writeFileSync(packagePath, str)
 })
--- server/scripts/server.cjs
+++ server/scripts/server.cjs
@@ -1,5 +1,6 @@
 // start.cjs
-import('../dist/server.mjs')
+const serverPath = resolve(__dirname, '../dist/server.mjs')
+import(serverPath)
   .then(mod => mod.default?.())
   .catch(err => {
     console.error(err);
